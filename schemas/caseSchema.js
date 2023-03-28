7a221f628d2d6d7be903b9a2270f1f01d4d09876
Gustav Clavin <gustav.clavin99@gmail.com>
2023-03-28 09:08:25 +0200

    a

--- schemas/caseSchema.js
+++ schemas/caseSchema.js
@@ -4,7 +4,7 @@ const caseSchema = mongoose.Schema({
     email:      {type: String, required: true},
     subject:    {type: String, required: true},
     message:    {type: String, required: true},
-    status:     {type: String, default: 1},
+    status:     {type: Number, default: 1},
     comment:    [{ type: mongoose.Schema.Types.ObjectId, ref:'Comment' }]
     },
     {timestamps: true})
